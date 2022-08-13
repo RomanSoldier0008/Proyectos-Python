5ceb708d79803de890ddd2276f0b926494083aa7
Marius <49422572+RomanSoldier0008@users.noreply.github.com>
2022-08-13 00:02:39 -0300

    Delete Multiplication.py

--- Multiplication.py
+++
@@ -1,9 +0,0 @@
-from Constants import RESULT, RESULT_EXTENSION
-
-class Multiplication:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
-
-    def multiply_and_return(self):
-        return RESULT.format(self.num1 * self.num2) + RESULT_EXTENSION
