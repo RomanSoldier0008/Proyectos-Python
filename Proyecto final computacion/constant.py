6b506bc8776f8663ffc1f66b1f4397552fd70562
Marius <49422572+RomanSoldier0008@users.noreply.github.com>
2023-03-21 21:02:15 -0300

    Combat_system_complete

---
+++ Proyecto final computacion/constant.py
@@ -0,0 +1,362 @@
+# INVENTORY
+POINTS_REST = "\nPuntos restantes para poder usar el item:"
+YOU_LACK_S = "Necesitas {} más de Fuerza"
+YOU_LACK_A = "Necesitas {} más en agilidad"
+YOU_LACK_V = "Necesitas {} más en Vitalidad"
+MENU_OPC_INVENTORY_INPUT = """
+1) Llevar item a mano
+2) Tienda
+3) Atrás
+Ingrese opción: """
+
+# TIENDA(ITEMS)
+MENU_OPC_SHOP_INPUT = """
+---------------
+1) Comprar
+2) Vender
+3) Atrás
+---------------
+Ingrese opción del menú: """
+NO_ITEMS_IN_SHOP = "Has comprado todos los items. ENHORABUENA"
+THE_ITEM_IS_EARLY = "No puedes vender los items iniciales"
+NO_MONEY_CHARACTER_SELECTED = "No llevas esa cantidad de dinero!"
+ITEM_HAS_BEEN_SOLD = """
+[ x1 {} Añadido al vault ]
+[ Monedas restantes {} ]"""
+ITEM_HAS_BEEN_SELL = """
+[ x1 {} Vendido ]
+[ Tus Monedas son: {} ]"""
+#       ENANO
+BALMUNG = "Balmung"
+BALMUNG_NECESSARY_STRENGTH = 25
+BALMUNG_NECESSARY_AGILITY = 20
+BALMUNG_NECESSARY_VITALITY = 10
+DAMAGE_BALMUNG = 10
+#       ELFO
+ASHBRINGER = "Ashbringer"
+ASHBRINGER_NECESSARY_STRENGTH = 20
+ASHBRINGER_NECESSARY_AGILITY = 25
+ASHBRINGER_NECESSARY_VITALITY = 10
+DAMAGE_ASHBRINGER = 10
+#       MAGO
+ORB_HELL_FIRE = "Orbe del fuego infernal"
+ORB_HELL_FIRE_NECESSARY_STRENGTH = 10
+ORB_HELL_FIRE_NECESSARY_AGILITY = 15
+ORB_HELL_FIRE_NECESSARY_VITALITY = 15
+DAMAGE_ORB_HELL_FIRE = 10
+
+# ITEMS
+#   MONEDA
+MONEYS = "Monedas"
+MONEY_TO_DROP = 369
+BONUS_MONEY_DIFFICULT_EASY = 1
+BONUS_MONEY_DIFFICULT_NORMAL = 2
+BONUS_MONEY_DIFFICULT_HARD = 3
+MONEY_HAS_BEEN_DROP = "[ Has obtenido {} monedas ]"
+MONEY_HAS_NOT_BEEN_DROP = "[ El enemigo no tenia dinero ]"
+#   ARMAS
+#       ENANO
+#           SHORT_SWORD
+SHORT_SWORD = "Espada corta"
+SHORT_SWORD_NECESSARY_STRENGTH = 4
+SHORT_SWORD_NECESSARY_AGILITY = 3
+SHORT_SWORD_NECESSARY_VITALITY = 2
+DAMAGE_SHORT_SWORD = 1
+#            WAR_AXE
+WAR_AXE = "Hacha de guerra"
+WAR_AXE_NECESSARY_STRENGTH = 10
+WAR_AXE_NECESSARY_AGILITY = 6
+WAR_AXE_NECESSARY_VITALITY = 8
+DAMAGE_WAR_AXE = 3
+#             DOOM HAMMER
+DOOM_HAMMER = "Martillo de perdición"
+DOOM_HAMMER_NECESSARY_STRENGTH = 10
+DOOM_HAMMER_NECESSARY_AGILITY = 6
+DOOM_HAMMER_NECESSARY_VITALITY = 8
+DAMAGE_DOOM_HAMMER = 3
+#       ELFO
+#              SHORT_BOW
+SHORT_BOW = "Arco corto"
+SHORT_BOW_NECESSARY_STRENGTH = 3
+SHORT_BOW_NECESSARY_AGILITY = 5
+SHORT_BOW_NECESSARY_VITALITY = 1
+DAMAGE_SHORT_BOW = 1
+#               Luna_bow
+LUNA_BOW = "Arco de Luna"
+LUNA_BOW_NECESSARY_STRENGTH = 6
+LUNA_BOW_NECESSARY_AGILITY = 10
+LUNA_BOW_NECESSARY_VITALITY = 8
+DAMAGE_LUNA_BOW = 3
+#               Lux_bow
+LUX_BOW = "Arco de Luz"
+LUX_BOW_NECESSARY_STRENGTH = 6
+LUX_BOW_NECESSARY_AGILITY = 10
+LUX_BOW_NECESSARY_VITALITY = 8
+DAMAGE_LUX_BOW = 3
+#       MAGO
+#               WALKING_STICK
+WALKING_STICK = "Bastón"
+WALKING_STICK_NECESSARY_STRENGTH = 2
+WALKING_STICK_NECESSARY_AGILITY = 4
+WALKING_STICK_NECESSARY_VITALITY = 5
+DAMAGE_WALKING_STICK = 1
+#               Soul Calibur
+SOUL_CALIBUR = "Alma Calibur"
+SOUL_CALIBUR_NECESSARY_STRENGTH = 6
+SOUL_CALIBUR_NECESSARY_AGILITY = 8
+SOUL_CALIBUR_NECESSARY_VITALITY = 10
+DAMAGE_SOUL_CALIBUR = 3
+#               GOOD GOODNESS BONE WAND
+STAFF_OF_PURE_GOODNESS = "Bastón de pura bondad"
+STAFF_OF_PURE_GOODNESS_NECESSARY_STRENGTH = 6
+STAFF_OF_PURE_GOODNESS_NECESSARY_AGILITY = 8
+STAFF_OF_PURE_GOODNESS_NECESSARY_VITALITY = 10
+DAMAGE_STAFF_OF_PURE_GOODNESS = 3
+# -----------------------------------------
+NUMBER_ITEM_INPUT = "Ingrese el número del item: "
+NO_DROP_ITEM = "[ El enemigo no dejó ningún item ]"
+ITEM_ADDED_TO_INVENTORY = "[ {} añadido al inventario ]"
+NO_ITEM_IN_INVENTORY = "No hay ningún item en el inventario"
+ITEM_BY_OTHER_BREED = "Este ítem es para la raza {}"
+
+# GAME
+
+CONSTANT_EXP_GOAL = 8
+SELECT_OPTION_PLAYER_INPUT = """
++-------------------------------------------|------------------------------+
+         ꧁ঔৣ☬✞{}✞☬ঔৣ꧂                         ꧁ঔৣ☬✞{}✞☬ঔৣ꧂
+
+──────────────▄───▄                                    Fuerza: {}
+──────────▄█▄─█▀█▀█─▄█▄                                Agilidad: {}
+─────────▀▀████▄█▄████▀▀                               Vida: {}
+──────────────▀█▀█▀
+                                                       Restantes: {}
+Raza: {}
+En mano: {}
+Fuerza: {}  Agilidad: {}  Vida: {}
+Experiencia: {}/{}  Nivel: {}
+Monedas: {}
++-------------------------------------------|------------------------------+
+
+1) Hacer disputa
+2) Cambiar de héroe
+3) Opciones del héroe
+4) Atrás
+Ingrese opción: """
+DIFFICULT_EASY = "Facil"
+DIFFICULT_NORMAL = "Normal"
+DIFFICULT_HARD = "Dificil"
+CONSTANT_EXP_EASY = 1
+CONSTANT_EXP_NORMAL = 1.5
+CONSTANT_EXP_HARD = 2
+DIFFICULT_INPUT = f"""
+|<>-<>-<>-<>-<>-<>SELECCIONE DIFICULTAD<>-<>-<>-<>-<>-<>-<<>-<>-<>-<>-<|
+|        --->     --->  [FÁCIL]    <---     <---  EXP AUMENTADA: {CONSTANT_EXP_HARD * 0}%    |
+|        --->     --->  [NORMAL]   <---     <---  EXP AUMENTADA: {(CONSTANT_EXP_HARD/CONSTANT_EXP_HARD) * 50}% |
+|        --->     --->  [DIFÍCIL]  <---     <---  EXP AUMENTADA: {(CONSTANT_EXP_HARD/CONSTANT_EXP_HARD) * 100}%|
+|<>-<>-<>-<>-<>-<><>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<<>-<>-<>-<>-<|
+Ingrese dificultad: """
+ENEMY_DEAD = "[ Enemigo derribado ]"
+HERO_DAMAGE = "[ {} realizó: {} de daño ]"
+HERO_DEAD = "[ {} fue derrotado ]"
+ENEMY_DAMAGE = "[ {} recibió: {} de daño ]"
+GAME_OVER = """
+¡Te has quedado sin héroes!
+
+-----------------
+      GAME
+      OVER
+-----------------"""
+GAME_WIN = """
+¡Has derrotado a todos los Enemigos!
+
+-----------------
+       HAS
+      GANADO
+-----------------"""
+SELECT_HERO_TO_ATTACK = "Seleccione al héroe {}: "
+LEVEL_UP = """
+--------------------------------------
+TU HÉROE SUBIÓ AL NIVEL <<|{}|>>
++5 fuerza
++5 agilidad
++5 vitalidad
+--------------------------------------"""
+SUM_ATTRIBUTE_WHEN_LEVEL_UP = 5
+
+# Enemigos
+
+ENEMY = "Enemigo"
+MAX_ATTRIBUTE_ENEMY_EASY = 5
+MIN_RANDOM_ATTRIBUTE_ENEMY_EASY = 1
+MAX_RANDOM_ATTRIBUTE_ENEMY_EASY = 2
+SUM_VITALITY_ENEMY_EASY = 1
+MAX_ATTRIBUTE_ENEMY_NORMAL = 25
+MIN_RANDOM_ATTRIBUTE_ENEMY_NORMAL = 6
+MAX_RANDOM_ATTRIBUTE_ENEMY_NORMAL = 10
+SUM_VITALITY_ENEMY_NORMAL = 5
+MAX_ATTRIBUTE_ENEMY_HARD = 100
+MIN_RANDOM_ATTRIBUTE_ENEMY_HARD = 25
+MAX_RANDOM_ATTRIBUTE_ENEMY_HARD = 37
+SUM_VITALITY_ENEMY_HARD = 20
+NUMBER_OF_ENEMIES = 10
+GENERATE_ENEMY = """
+|<>-<>-<>-<>-<>-<>GENERADOR DE ENEMIGO<>-<>-<>-<>-<>-<>-<
+|        --->     --->  {}  <---     <---          
+|<>-<>-<>-<>-<>-<><>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-<>-"""
+RETURN_ENEMIES_FORMAT = """
+********************************
+* {}
+* Raza: {}
+* Fuerza: {}
+* Agilidad: {}
+* Vitalidad: {}
+********************************"""
+
+# Héroe
+
+# ----------------------------------------------------------------------------------------------------------------------
+HERO_HAS_BEEN_DELETE = "Héroe borrado"
+ERROR_MAX_LIMIT_CREATE_CHARACTER = "Error: Has alcanzado el límite(3) de Héroes creados."
+MAX_LIMIT_CREATE_CHARACTER = 2  # es 3 por el 0
+HERO = "Héroe"
+DELETE_CHARACTER_OF_LIST_NAME_INPUT = "Ingrese el nombre del héroe que quiere borrar {}: "
+RETURN_HERO_FORMAT = """
+********************************
+* {}                           
+* Raza: {}                     
+* Nombre: {}                   
+* Fuerza: {}                   
+* Agilidad: {}                 
+* Vitalidad: {}                
+* Exp: {}                      
+* Nivel: {}                    
+********************************"""
+BREED_ELF = "Elfo"
+BREED_WIZARD = "Mago"
+BREED_DWARF = "Enano"
+CHARACTER_HERO_INPUT = f"""
+----------------------------
+- {BREED_ELF}
+- {BREED_WIZARD}
+- {BREED_DWARF}
+----------------------------
+Seleccione el personaje: """
+MAX_ATTRIBUTE_HERO = 15
+CHARACTER_NAME_HERO_INPUT = "Ingrese nombre: "
+REMAINDER = "Puntos restantes: {}"
+STRENGTH_HERO_INPUT = "Ingrese fuerza: "
+AGILITY_HERO_INPUT = "Ingrese agilidad: "
+VITALITY_HERO_INPUT = "Ingrese vitalidad: "
+EXP_STARTED = 0
+LEVEL_STARTED = 1
+POWER_START_ELF = 2
+POWER_START_WIZARD = 2
+POWER_START_DWARF = 2
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# MENÚ
+
+OPTIONS_MENU_INPUT = """
+                                                                                          ────────────────────────────────
+ •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•───────────────██████████───────
+:::       *...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*       ::: ──────────────████████████──────
+*:                                                                                     :* ──────────────██────────██──────
+*:                           :::::::::::::::::::::::::::::::::::                       :* ──────────────██▄▄▄▄▄▄▄▄▄█──────
+*:                           :::::::::::::::::::::::::::::::::::                       :* ──────────────██▀███─███▀█──────
+*:                           :::::::::::::::::::::::::::::::::::                       :* █─────────────▀█────────█▀──────
+*:                       [1] ::::::::::::: Comenzar :::::::::::: [1]                   :* ██──────────────────█───────────
+*:                       [2] ::::::::::: Crear Héroe ::::::::::: [2]                   :* ─█──────────────██──────────────
+*:                       [3] ::::::::::: Borrar Héroe :::::::::: [3]                   :* █▄────────────████─██──████
+*:                       [4] :::::::::::: Créditos ::::::::::::: [4]                   :* ─▄███████████████──██──██████ ──
+*:                       [5] :::::::::::::: Salir :::::::::::::: [5]                   :* ────█████████████──██──█████████
+*:                           :::::::::::::::::::::::::::::::::::                       :* ─────────────████──██─█████──███
+*:                           :::::::::::::::::::::::::::::::::::                       :* ──────────────███──██─█████──███
+*:                           :::::::::::::::::::::::::::::::::::                       :  ──────────────██─────████████▀
+*:                                                                                     :* ────────────────██████████
+:::       *...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*...*       ::: ────────────────██████████
+  •.¸¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸¸.•¨   ─────────────────████████
+                                                                                          ───────────────────██████████▄
+                                                                                          ────────────────────█████████▀
+                                                                                          ─────────────────────████──███
+                                                                                          ────────────────────▄████▄──██
+                                                                                          ────────────────────██████───▀
+                                                                                          ────────────────────▀▄▄▄▄▀
+Ingrese opción: """
+
+MENU_GAME_INPUT = """
+--------------------------------------------------------------------------------------
+       OPCIONES       |          MENÚ JUEGO        |     ESTADÍSTICAS
+                      |            .---.           |  
+1) Jugar              |           | --- |          | 
+                      |           | --- |          |
+                      |           | --- |          | Dificultad: {}
+                      |     .----- ^ - ^ -----.    |
+                      |     :     S.P.Q.R     :    |
+                      |     :_________________:    | 
+                      |          |   //  |         |
+                      |          |   //  |         | Héroes: {}
+2) Elegir dificultad  |          |   //  |         |
+                      |          |   //  |         |
+                      |          |   //  |         | 
+                      |          |   //  |         |
+                      |          |   //  |         | Drop de armas: {}%
+                      |          |   //  |         | 
+                      |          \  * *  /         |
+                      |           \  *  /          | 
+                      |            \V V/           | 
+3) Atrás              |              V             | Drop de monedas: {}%
+--------------------------------------------------------------------------------------
+Ingrese opción: """
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Créditos
+
+UNIVERSITY = """
+                                                                                                      -----------------------
+                                                                                                      Universidad de Mendoza
+                                                                                                      Facultad de Ingeniería 
+                                                                                                      -----------------------"""
+THANKS = "Agradecimientos..."
+THANKS_BYE = """
+                                                 .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.
+                                                | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+                                                | |    ______    | || |  _______     | || |      __      | || |     ______   | || |     _____    | || |      __      | || |    _______   | |
+                                                | |  .' ___  |   | || | |_   __ \    | || |     /  \     | || |   .' ___  |  | || |    |_   _|   | || |     /  \     | || |   /  ___  |  | |
+                                                | | / .'   \_|   | || |   | |__) |   | || |    / /\ \    | || |  / .'   \_|  | || |      | |     | || |    / /\ \    | || |  |  (__ \_|  | |
+                                                | | | |    ____  | || |   |  __ /    | || |   / ____ \   | || |  | |         | || |      | |     | || |   / ____ \   | || |   '.___`-.   | |
+                                                | | \ `.___]  _| | || |  _| |  \ \_  | || | _/ /    \ \_ | || |  \ `.___.'\  | || |     _| |_    | || | _/ /    \ \_ | || |  |`\____) |  | |
+                                                | |  `._____.'   | || | |____| |___| | || ||____|  |____|| || |   `._____.'  | || |    |_____|   | || ||____|  |____|| || |  |_______.'  | |
+                                                | |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+                                                | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+                                                 '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'
+"""
+TEACHER1 = "Profesor: Jose Ramon Novas"
+TEACHER2 = "Profesor: Fabrizio Piruzi"
+OP1_CREDIT = "Alumno: Francisco Merenda"
+OP2_CREDIT = "Alumno: Juan Imaz"
+OP3_CREDIT = "Alumno: Juan Michaux"
+
+CREDITS_PRINT = """
+                                                                                                      *♥.•´¸.•*´✶´♡ ¸.•*´´♡*💚˚*
+                                                                                                      *_○💙_   Créditos    💙*˚*
+                                                                                                      *💚.•´¸.•*´✶´♡ ¸.•*´´♡⛅*
+                                                                                                      *° ☆ ° ˛*˛☆_Π____*_*˚☆*
+                                                                                                      *˚ ˛★˛•˚ */______/ ~＼。˚ ˚
+                                                                                                      *˚ ˛•˛•˚🌈｜ 田田 ｜門｜ ˚*
+                                                                                                      *🌴╬═🌴╬╬🌴╬╬🌴═╬╬═🌴*╬╬🌴"""
+
+# ERRORES
+
+# ----------------------------------------------------------------------------------------------------------------------
+ERROR_IN_POINTS_HERO = "ERROR: Has ingresado mal los puntos. Intente de nuevo"
+ERROR_IN_OPC_MENU = "ERROR: Intente ingresar una opción del menú"
+ERROR_IN_BREED_HERO = "ERROR: Ingresó mal la raza. Intente de nuevo"
+ERROR_IN_ADD_CHARACTER_TO_LIST = "ERROR: No puedes crear más de 3 héroes"
+ERROR_NO_HEROS = "ERROR: No hay ningún héroe creado"
+ERROR_IN_SELECTION_DIFFICULT = "ERROR: Ingresaste una opción inválida. Intente de nuevo"
+ERROR_START_NO_DIFFICULT = "ERROR: No has elegido una dificultad."
+ERROR_IN_SELECT_HERO = "ERROR: Ingresaste mal el nombre del héroe"
+ERROR_IN_DELETE_HERO = "ERROR: No se encontró el héroe especificado."
+ERROR_NO_ITEM_WHICH_THIS_NUMBER = "ERROR: No hay item con ese número"
